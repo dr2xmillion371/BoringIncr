8b4b04c9fe2eaac50f53f9b8e5c75192bffc7d20
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-05-10 17:55:22 -0500

    Add files via upload

---
+++ BoringIncremental.py
@@ -0,0 +1,26 @@
+# Boring Incremental
+
+
+
+import time
+
+print("Welcome to Boring incremental!")
+
+
+
+
+def give_user_increments(): 
+    incr_list=['You have: 1.0 dogs', 'You have: 1.25 dogs', 'You have 56 dogs',
+    'You have: 5.645e+31 dogs', 'You have: 4.635343852e+63534 dogs']
+    for i in incr_list:
+      print(i)
+      time.sleep(1.255)
+      
+      
+
+give_user_increments()
+
+
+
+
+# END
